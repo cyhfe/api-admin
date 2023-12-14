2fe2996377dc8f42fd7e20cfcbd5833ba540081b
chenyuhao <cccchenyuhao@gmail.com>
2023-12-14 15:14:25 +0800

    login forn

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,9 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Link as RouterLink, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../Auth";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Controller, useForm } from "react-hook-form";
+import Link from "@mui/material/Link";
 
 export default function Login() {
   const { user } = useAuth("Login");
@@ -20,7 +21,7 @@ export default function Login() {
 }
 
 function LoginForm() {
-  const { handleSubmit, reset, setValue, control } = useForm({
+  const { handleSubmit, control } = useForm({
     defaultValues: {
       username: "",
       password: "",
@@ -28,57 +29,73 @@ function LoginForm() {
   });
   return (
     <div className="flex justify-center items-center h-screen">
-      <form
-        onSubmit={handleSubmit((data) => console.log(data))}
-        className="flex flex-col gap-y-5"
-      >
-        <Controller
-          rules={{
-            required: {
-              value: true,
-              message: "请输入用户名",
-            },
-            minLength: {
-              value: 6,
-              message: "用户名不能少于6位",
-            },
-          }}
-          render={({ field, fieldState: { error } }) => {
-            console.log(error);
-            return (
+      <div className="flex flex-col gap-y-5">
+        <form
+          onSubmit={handleSubmit((data) => console.log(data))}
+          className="flex flex-col gap-y-5"
+        >
+          <Controller
+            rules={{
+              required: {
+                value: true,
+                message: "请输入用户名",
+              },
+              minLength: {
+                value: 6,
+                message: "用户名不能少于6位",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => {
+              return (
+                <TextField
+                  label="用户名"
+                  size="small"
+                  error={!!error}
+                  helperText={error?.message}
+                  {...field}
+                />
+              );
+            }}
+            name="username"
+            control={control}
+          />
+          <Controller
+            rules={{
+              required: {
+                value: true,
+                message: "请输入密码",
+              },
+              minLength: {
+                value: 6,
+                message: "密码不能少于6位",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
               <TextField
-                label="用户名"
+                type="password"
                 size="small"
+                label="密码"
                 error={!!error}
                 helperText={error?.message}
                 {...field}
               />
-            );
-          }}
-          name="username"
-          control={control}
-        />
-        <Controller
-          rules={{
-            required: {
-              value: true,
-              message: "请输入密码",
-            },
-            minLength: {
-              value: 6,
-              message: "密码不能少于6位",
-            },
-          }}
-          render={({ field }) => (
-            <TextField type="password" size="small" label="密码" {...field} />
-          )}
-          name="password"
-          control={control}
-        />
-        <Button type="submit" variant="outlined">
-          登陆
-        </Button>
-      </form>
+            )}
+            name="password"
+            control={control}
+          />
+          <Button type="submit" variant="outlined">
+            登陆
+          </Button>
+        </form>
+        <div>
+          <span>
+            还没有账号？去
+            <Link component={RouterLink} to="/register">
+              注册
+            </Link>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
